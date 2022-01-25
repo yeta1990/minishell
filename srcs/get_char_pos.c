f2dfb26690b1600491ec6c46eb256a9925d9a0ab
albgarci <albgarci@student.42madrid.com>
2022-01-25 18:44:23 +0100

    norm: has closed quotes in get_char_pos.c

--- srcs/get_char_pos.c
+++ srcs/get_char_pos.c
@@ -6,7 +6,7 @@
 /*   By: crisfern <crisfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 13:55:09 by albgarci          #+#    #+#             */
-/*   Updated: 2022/01/19 17:09:23 by albgarci         ###   ########.fr       */
+/*   Updated: 2022/01/25 18:43:39 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,31 +32,25 @@ int	has_closed_quotes(char *str)
 {
 	int		smpl;
 	int		dble;
-	char	*aux;
 	int		close;
 
 	smpl = get_char_pos(str, '\'');
 	dble = get_char_pos(str, '\"');
-//	printf("jeje");
 	if (get_char_pos(str, '|') < smpl && get_char_pos(str, '|') < dble)
 		return (0);
 	if ((smpl > 0 && dble > 0 && smpl < dble) || (smpl > 0 && dble <= 0))
 	{
-		aux = str + smpl;
-		close = get_char_pos(aux, '\'');
-		if (close == (int) ft_strlen(aux) + 1)
+		close = get_char_pos(str + smpl, '\'');
+		if (close == (int) ft_strlen(str + smpl) + 1)
 			return (has_closed_quotes(str + smpl));
-		else
-			return (1);
+		return (1);
 	}
 	else if ((smpl > 0 && dble > 0 && dble < smpl) || (dble > 0 && smpl <= 0))
 	{
-		aux = str + dble;
-		close = get_char_pos(aux, '\"');
-		if (close == (int) ft_strlen(aux) + 1)
+		close = get_char_pos(str + dble, '\"');
+		if (close == (int) ft_strlen(str + dble) + 1)
 			return (has_closed_quotes(str + dble));
-		else
-			return (2);
+		return (2);
 	}
 	return (0);
 }
@@ -71,9 +65,11 @@ void	cut_outside_quotes(char *aux, t_files **full_strings, char c)
 		while (aux && *aux && *aux == ' ')
 			aux++;
 		if (get_char_pos(aux + 1, ' ') < get_char_pos(aux + 1, c) && *aux != c)
-			ft_lstadd_back_files(full_strings, ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, ' '))));
+			ft_lstadd_back_files(full_strings,
+				ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, ' '))));
 		else if (*aux != c)
-			ft_lstadd_back_files(full_strings, ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, c))));
+			ft_lstadd_back_files(full_strings,
+				ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, c))));
 		while (aux && *aux && *aux != ' ' && *aux != c)
 			aux++;
 	}
@@ -88,50 +84,9 @@ void	cut_end_quotes(char *aux, t_files **full_strings)
 	{
 		while (aux && *aux && *aux == ' ')
 			aux++;
-		ft_lstadd_back_files(full_strings, ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, ' '))));
+		ft_lstadd_back_files(full_strings,
+			ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, ' '))));
 		while (aux && *aux && *aux != ' ')
 			aux++;
 	}
 }
-/*
-char	**split_quote_sensitive(char *str)
-{
-	int		quotes_type;
-	char	*aux;
-	t_files	**full_strings;
-
-	full_strings = malloc(sizeof(t_files *));
-	full_strings[0] = 0;
-	aux = str;
-	quotes_type = has_closed_quotes(aux);
-	while (quotes_type != 0)
-	{
-		if (quotes_type == 1)
-		{
-			cut_outside_quotes(aux, full_strings, '\'');
-			while (*aux != '\'')
-				aux++;
-			if (has_closed_quotes(aux) == 1)
-				ft_lstadd_back_files(full_strings, ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, '\'') + 1)));
-			aux++;
-			while (*aux != '\'')
-				aux++;
-			aux++;
-		}
-		else if (quotes_type == 2)
-		{
-			cut_outside_quotes(aux, full_strings, '\"');
-			while (*aux != '\"')
-				aux++;
-			if (has_closed_quotes(aux) == 2)
-				ft_lstadd_back_files(full_strings, ft_lstnew(ft_substr(aux, 0, get_char_pos(aux + 1, '\"') + 1)));
-			aux++;
-			while (*aux != '\"')
-				aux++;
-			aux++;
-		}
-		quotes_type = has_closed_quotes(aux);
-	}
-	cut_end_quotes(aux, full_strings);
-	return (from_list_to_double_char(full_strings));
-}*/
