3e777300f78e8481749ba1fcaff3c879c57f584a
albgarci <albgarci@student.42madrid.com>
2022-01-27 12:53:04 +0100

    norm fix

--- srcs/quote_parser.c
+++ srcs/quote_parser.c
@@ -6,7 +6,7 @@
 /*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 12:09:36 by albgarci          #+#    #+#             */
-/*   Updated: 2022/01/27 12:51:08 by albgarci         ###   ########.fr       */
+/*   Updated: 2022/01/27 12:52:51 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,8 @@ void	split_ops_with_quotes(t_split_cmds_vars *v, t_data *data)
 		v->expanded = expansor(&v->trimmed, 2, data);
 	ft_memcpy(v->result, v->expanded, ft_strlen(v->expanded));
 	v->result += ft_strlen(v->expanded);
-	if (get_next_separator(v->aux + 1, what_quotes(v->flag)) == (int) ft_strlen(v->aux + 1) + 1)
+	if (get_next_separator(v->aux + 1,
+			what_quotes(v->flag)) == (int) ft_strlen(v->aux + 1) + 1)
 		v->aux += v->forward;
 	else
 		v->aux += v->forward + 1;
