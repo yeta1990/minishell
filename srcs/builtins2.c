f2cd2892db3bebabe4a1be6b0cbafe41d4b70e60
albgarci <albgarci@student.42madrid.com>
2022-02-01 17:34:31 +0100

    fix: more export and unset cases

--- srcs/builtins2.c
+++ srcs/builtins2.c
@@ -6,7 +6,7 @@
 /*   By: crisfern <crisfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 13:32:33 by albgarci          #+#    #+#             */
-/*   Updated: 2022/02/01 17:16:43 by albgarci         ###   ########.fr       */
+/*   Updated: 2022/02/01 17:26:43 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,29 +107,30 @@ void	update_env(t_data *data, int index_exp, int i)
 //si contiene no alphanum, error
 //si contiene un '$' (es decir, que el expander haya dejado la '$' tal cual, también error
 
-int	export_error_checker(t_data *data, t_cmd *cmd)
+int	export_error_checker(t_data *data, char *cmd_complete)
 {
-	int	i;
+//	int	i;
 	int	j;
 	int	equals;
 
-	i = 1;
-	equals = 0;
-	while (cmd->cmd_complete[i])
-	{
+//	i = 1;
+//	equals = 0;
+//	while (cmd->cmd_complete[i])
+//	{
 		j = 0;
-		if (ft_strlen(cmd->cmd_complete[i]) > 0 && ft_isalpha(cmd->cmd_complete[i][0]) == 0)
-			return (export_error(cmd->cmd_complete[i], data));
-		while (cmd->cmd_complete[i][j])
+		equals = 0;
+		if (ft_strlen(cmd_complete) > 0 && ft_isalpha(cmd_complete[0]) == 0)
+			return (export_error(cmd_complete, data));
+		while (cmd_complete[j])
 		{
-			if (cmd->cmd_complete[i][j] == '=' && equals == 0)
+			if (cmd_complete[j] == '=' && equals == 0)
 				equals = 1;
-			if (equals == 0 && ft_isalnum(cmd->cmd_complete[i][j]) == 0)
-				return (export_error(cmd->cmd_complete[i], data));
+			if (equals == 0 && ft_isalnum(cmd_complete[j]) == 0)
+				return (export_error(cmd_complete, data));
 			j++;
 		}
-		i++;
-	}
+//		i++;
+//	}
 	return (0);
 }
 
@@ -137,7 +138,9 @@ int	export_builtin(t_data *data, t_cmd *cmd)
 {
 	int		i;
 	int		index_exp;
-	
+	int		return_code;
+
+	return_code = 0;	
 	i = 0;
 	if (!cmd->cmd_complete[1] || (cmd->cmd_complete[1] && ft_strlen(cmd->cmd_complete[1]) == 0))
 	{
@@ -150,20 +153,23 @@ int	export_builtin(t_data *data, t_cmd *cmd)
 		data->last_code = 0;
 		return (0);
 	}
-	if (export_error_checker(data, cmd))
-		return (1);
 	while (cmd->cmd_complete[++i])
 	{
-		index_exp = search_word(data->exp, cmd->cmd_complete[i]);
-		if (ft_strchr(cmd->cmd_complete[i], '='))
-			update_env(data, index_exp, i);
+		if (export_error_checker(data, cmd->cmd_complete[i]))
+			return_code = 1;
 		else
-			if (index_exp < 0)
-				data->exp = add_entry(data->exp,
+		{
+			index_exp = search_word(data->exp, cmd->cmd_complete[i]);
+			if (ft_strchr(cmd->cmd_complete[i], '='))
+				update_env(data, index_exp, i);
+			else
+				if (index_exp < 0)
+					data->exp = add_entry(data->exp,
 						ft_strdup(cmd->cmd_complete[i]));
+		}
 	}
-	data->last_code = 0;
-	return (0);
+	data->last_code = return_code;
+	return (return_code);
 }
 
 int	unset_builtin(t_data *data, t_cmd *cmd)
@@ -171,19 +177,24 @@ int	unset_builtin(t_data *data, t_cmd *cmd)
 	int		index_env;
 	int		index_exp;
 	int		i;
+	int		return_code;
 
+	return_code = 0;
 	i = 1;
-	if (export_error_checker(data, cmd))
-		return (1);
 	while (cmd->cmd_complete[i])
 	{
-		index_env = search_word_del(data->env, cmd->cmd_complete[i]);
-		index_exp = search_word_del(data->exp, cmd->cmd_complete[i]);
-		if (index_exp)
-			data->exp = del_entry(data->exp, index_exp);
-		if (index_env)
-			data->env = del_entry(data->env, index_env);
+		if (export_error_checker(data, cmd->cmd_complete[i]))
+			return_code = 1;
+		else
+		{
+			index_env = search_word_del(data->env, cmd->cmd_complete[i]);
+			index_exp = search_word_del(data->exp, cmd->cmd_complete[i]);
+			if (index_exp)
+				data->exp = del_entry(data->exp, index_exp);
+			if (index_env)
+				data->env = del_entry(data->env, index_env);
+		}
 		i++;
 	}
-	return (0);
+	return (return_code);
 }
