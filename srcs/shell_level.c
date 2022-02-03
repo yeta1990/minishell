c4b7e490b08e7e3e8e958273529ce91bfc9353a8
albgarci <albgarci@student.42madrid.com>
2022-02-03 16:17:50 +0100

    fix: leaks in shell level

--- srcs/shell_level.c
+++ srcs/shell_level.c
@@ -6,7 +6,7 @@
 /*   By: crisfern <crisfern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 15:08:10 by crisfern          #+#    #+#             */
-/*   Updated: 2022/02/02 10:14:06 by albgarci         ###   ########.fr       */
+/*   Updated: 2022/02/03 16:17:09 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,20 @@ void	inc_shell_level(void)
 	char	*str;
 	int		num;
 	int		i;
+	char	*num_string;
 
+	num_string = 0;
 	i = search_word(g_data.env, "SHLVL");
 	str = ft_strchr(g_data.env[i], '=');
 	num = ft_atoi(str + 1) + 1;
 	free(g_data.env[i]);
-	g_data.env[i] = ft_strjoin("SHLVL=", ft_itoa(num));
+	num_string = ft_itoa(num);
+	g_data.env[i] = ft_strjoin("SHLVL=", num_string);
 	i = search_word(g_data.exp, "SHLVL");
 	str = ft_strchr(g_data.exp[i], '=');
 	free(g_data.exp[i]);
-	str = ft_strjoin("SHLVL=\"", ft_itoa(num));
+	str = ft_strjoin("SHLVL=\"", num_string);
+	free(num_string);
 	g_data.exp[i] = ft_strjoin(str, "\"");
 	free(str);
 }
